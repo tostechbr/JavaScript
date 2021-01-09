b998d8c05c0c6f94f3e669d09394415e3cad8f76
Tiago Oliveira <tiagolivesan@outlook.com>
2021-01-09 12:35:12 -0300

    JS curso em vídeo

---
+++ Curso em video/ex009.js
@@ -0,0 +1,10 @@
+function calcular() {
+    var txtv = document.querySelector('input#txtvel')
+    var res = document.querySelector('div#res')
+    var vel = Number(txtv.value)
+    res.innerHTML = `<p>Sua velocidade atual é de <strong>${vel}Km/h</strong></p>`
+    if (vel > 60) {
+        res.innerHTML += `<p>Você está <strong> MULTADO </strong> por excesso de velocidade</p>`
+    }
+    res.innerHTML += `<p>Dirija sempre com cinto de segurança!</p>`
+}
