baaa49711f67520b759ca7d769f00cd29a483b3d
Tiago Oliveira <tiagolivesan@outlook.com>
2021-01-09 14:21:29 -0300

    JS curso em vídeo

--- Curso em video/ex010.js
+++ Curso em video/ex010.js
@@ -1,8 +1,8 @@
 var idade = 22 
-if (idade < 18){
-    console.log('Menor de idade')
-}else {
-    if (idade >= 16 && idade < 18){
+if (idade < 16){
+    console.log('Não vota')
+} else {
+    if (idade >= 16 && idade < 18) {
         console.log('Voto opcional')
     }
 }
