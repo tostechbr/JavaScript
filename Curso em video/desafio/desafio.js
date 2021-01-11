0f2328979f022d07167961dbe6388a0eb0bf05e0
Tiago Oliveira <tiagolivesan@outlook.com>
2021-01-11 18:59:02 -0300

    JS curso em vídeo

---
+++ Curso em video/desafio/desafio.js
@@ -0,0 +1,22 @@
+function carregar () {
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('imagem')
+    var data = new Date()
+    var hora = data.getHours()
+    
+    msg.innerHTML = `Agora são ${hora} horas.`
+    if(hora >= 0 && hora < 12) {
+        // BOM DIA
+        img.src = 'dia.jpg'
+        document.body.style.backgroundColor = '#E0DAFF'
+    } else if (hora >= 12 && hora <= 18){
+        // BOA TARDE
+        img.src = 'tarde.jpg'
+        document.body.style.backgroundColor = '#CCE7F8'
+    } else {
+        //BOA NOITE
+        img.src = 'noite.jpg'
+        document.body.style.backgroundColor = '#D6FAF7'
+    }
+
+}
