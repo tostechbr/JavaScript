b035259a086e59d0d138b8c37dcbe73bd1284b64
Tiago Oliveira <tiagolivesan@outlook.com>
2021-01-12 18:43:05 -0300

    Update desafio2.js

--- Curso em video/desafio/desafio2.js
+++ Curso em video/desafio/desafio2.js
@@ -6,6 +6,35 @@ function verificar () {
     if (fano.value.length == 0 || fano.value > ano){
         window.alert('[ERRO] Verifique os dados e tente novamente!')
     }else {
-        window.alert('tudo ok')
+        var fsex = document.getElementsByName('radsex')
+        var idade = ano - Number(fano.value)
+        var gênero = ''
+        var img = document.createElement('img')
+        img.setAttribute('id', 'foto') // HTML <img id = "foto">
+        if (fsex[0].checked){
+            gênero = 'Homem'
+            if (idade >=0 && idade < 10){
+                // criança
+            }else if ( idade < 21){
+                // jovem
+            }else if ( idade < 50){
+                // adulto
+            }else {
+                // idoso
+            }
+        }else if (fsex[1].checked){
+            gênero = 'Mulher'
+            if (idade >=0 && idade < 10){
+                // criança
+            }else if ( idade < 21){
+                // jovem
+            }else if ( idade < 50){
+                // adulto
+            }else {
+                // idoso
+            }
+        }
+        res.style.textAlign = 'center'
+        res.innerHTML = `Detectamos ${gênero} com ${idade} anos.`
     }
 }
