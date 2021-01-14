1e38ab4fb72ab288429bf0f8d6cb814e02bc74eb
Tiago Oliveira <tiagolivesan@outlook.com>
2021-01-13 22:08:00 -0300

    Create ambiente.js
    Aprendendo "while" e "do" termos esse que ajuda na repetição!

---
+++ Curso em video/ambiente.js
@@ -0,0 +1,12 @@
+/* usando "while" para repetição 
+/*var c = 1
+while (c <= 5) {           
+    console.log(`Passo ${c}`)
+    c++
+}*/
+/* usando "do" para repetição  */
+var c = 1 
+do {
+    console.log(`Passo ${c}`)
+    c++
+} while (c <= 6)
