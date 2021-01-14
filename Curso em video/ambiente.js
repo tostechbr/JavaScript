281b59722d7c291f6ff1e89b2a0b1f7b39ba607c
Tiago Oliveira <tiagolivesan@outlook.com>
2021-01-14 19:59:29 -0300

    add for

--- Curso em video/ambiente.js
+++ Curso em video/ambiente.js
@@ -4,9 +4,21 @@ while (c <= 5) {
     console.log(`Passo ${c}`)
     c++
 }*/
-/* usando "do" para repetição  */
+
+//--------------
+
+/* usando "do" para repetição  
 var c = 1 
 do {
     console.log(`Passo ${c}`)
     c++
-} while (c <= 6)
+} while (c <= 6)*/
+
+//--------------
+
+/* usando "for" para repetição */
+console.log('Vai começar...')
+for (var c = 1; c <= 6; c++){
+    console.log(c)
+}
+console.log('FIM!')
