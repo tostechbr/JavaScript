894e4935c71225bf7e3314a821005a8e84cfcb4c
Tiago Oliveira <tiagolivesan@outlook.com>
2021-04-06 17:35:03 -0300

    JavaScript

---
+++ Luiz Otávio - Udemy/index.js
@@ -0,0 +1,12 @@
+//TESTING
+
+const nome = 'Tiago Oliveira'
+const sobrenome = 'Santos'
+const idade = 23;
+const peso = 76;
+const altura = 1.79;
+let imc;
+let anoNascimento;
+
+imc = peso / (altura*altura)
+anoNascimento = 2021 - idade;
